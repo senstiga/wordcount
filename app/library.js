674077980808daca242d2e896b5d3bf92896ce22
Ezenwajiaku Nnaemeka <nnaemekaezenwajiaku@gmail.com>
2017-01-12 20:35:54 +0100

    initial commit

---
+++ app/library.js
@@ -0,0 +1,18 @@
+'use strict'
+var file = {}
+
+
+     file.words = function (words) {
+         var split_string = words.trim().split()
+         var results = {}
+         for (var i = 0; i < split_string.length; i++) {
+             if (!results.hasOwnProperty(split_string[i])) {
+                 results[split_string[i]] = 1
+             } else {
+                 results[split_string[i]] += 1
+             }
+         }
+         return results
+     }
+  
+  module.exports = file;
